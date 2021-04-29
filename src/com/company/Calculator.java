eabc37257e32efe847e2d1b8cea73ce1bdbc28d5
ksn0325 <ksn0325@nate.com>
2021-04-30 08:01:08 +0900

    0430 calc start

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Calculator {
+    String str;
+    public Calculator() {
+    }
+    public Calculator(String str) {
+        this.str = str;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String exp [] = {"2+3=5*6+8/2", "9+4*2#7"};
+
+	    Calculator cal;
+
+	    for (int i = 0; i < exp.length; i++){
+            cal = new Calculator(exp[i]);
+        }
+
     }
 }
